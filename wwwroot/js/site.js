8a0be12a77166b98243d61ebed069bb47d067b39
pepijnemmers <pepijn.emmers@hotmail.com>
2024-10-14 23:18:14 +0200

    created darkmode toggle

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,7 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿// darkmode toggle
+document.getElementById("darkmode-toggle").addEventListener("click", function () {
+    document.documentElement.setAttribute(
+        "data-theme", 
+        document.documentElement.getAttribute("data-theme") === "dark" ? "light" : "dark"
+    )}
+);
