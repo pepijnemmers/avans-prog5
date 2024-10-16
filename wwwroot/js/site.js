8edcff4495cfafa4238b4b0d9c808064c660eff2
pepijnemmers <pepijn.emmers@hotmail.com>
2024-10-17 01:15:55 +0200

    UI updates

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,7 +1,13 @@
-﻿// darkmode toggle
+﻿// tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// darkmode toggle
 document.getElementById("darkmode-toggle").addEventListener("click", function () {
     document.documentElement.setAttribute(
         "data-theme", 
         document.documentElement.getAttribute("data-theme") === "dark" ? "light" : "dark"
     )}
 );
+
+
