8371585b8c9b2a906532f767be7fd3878ad4ca94
pepijnemmers <pepijn.emmers@hotmail.com>
2024-10-17 02:35:45 +0200

    route tryouts

--- Controllers/InventoryController.cs
+++ Controllers/InventoryController.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
+using NinjaApp.Models;
 
 namespace NinjaApp.Controllers;
 
 public class InventoryController : MainController
 {
+    [Route("Inventory/{ninjaId}")][Route("Inventory/Index/{ninjaId}")]
     public IActionResult Index(Guid ninjaId)
     {
-        return View();
+        Ninja? ninja = Context.Ninjas.Find(ninjaId);
+        return View(ninja);
     }
     
     public IActionResult Edit(Guid ninjaId)
