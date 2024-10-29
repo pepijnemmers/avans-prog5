fc9ff0a832e90fe431822f5ad0424816f37b3e51
pepijnemmers <pepijn.emmers@hotmail.com>
2024-10-29 18:12:18 +0100

    theme bug fix

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -5,9 +5,18 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
 // darkmode toggle
 document.getElementById("darkmode-toggle").addEventListener("click", function () {
     document.documentElement.setAttribute(
-        "data-theme", 
+        "data-theme",
         document.documentElement.getAttribute("data-theme") === "dark" ? "light" : "dark"
-    )}
-);
+    )
+    document.cookie = "theme=" + document.documentElement.getAttribute("data-theme") + "; path=/; max-age=31536000";
+});
 
+// set theme from cookie
+const theme = document.cookie.replace(/(?:(?:^|.*;\s*)theme\s*=\s*([^;]*).*$)|^.*$/, "$1");
+if (theme) {
+    document.documentElement.setAttribute("data-theme", theme);
+} else {
+    document.cookie = "theme=dark; path=/; max-age=31536000";
+    document.documentElement.setAttribute("data-theme", "dark");
+}
 
