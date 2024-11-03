ea36209912f4a4bdf1bd40a527d10ffca45945d3
pepijnemmers <pepijn.emmers@hotmail.com>
2024-11-03 15:42:56 +0100

    emtpy equipment show message

--- ViewModels/EquipmentViewModel.cs
+++ ViewModels/EquipmentViewModel.cs
@@ -5,6 +5,6 @@ namespace NinjaApp.ViewModels;
 
 public class EquipmentViewModel
 {
-    public required List<Equipment> Equipments { get; set; }
+    public required List<Equipment>? Equipments { get; set; }
     public required List<SlotCategory> SlotCategories { get; set; }
 }
